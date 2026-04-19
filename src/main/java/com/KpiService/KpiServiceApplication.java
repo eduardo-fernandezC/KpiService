e730c27fe41108081db9ce307c838420c09288f7
Eduardo Fernandez <ed.fernandezc@duocuc.cl>
2026-04-18 22:03:59 -0400

    first commit

---
+++ src/main/java/com/KpiService/KpiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.KpiService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/KpiService/KpiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KpiService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
