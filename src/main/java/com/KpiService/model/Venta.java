9e8fd6ed2d24be319a67f5e0eb2eb297a4ad2433
Eduardo Fernandez <ed.fernandezc@duocuc.cl>
2026-04-20 16:46:23 -0400

    fix: nombre de id renombrado

--- src/main/java/com/KpiService/model/Venta.java
+++ src/main/java/com/KpiService/model/Venta.java
@@ -2,52 +2,19 @@ package com.KpiService.model;
 
 import java.time.LocalDate;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Venta {
 
-    private Long id;
+    private Long idVenta;
     private Double total;
     private LocalDate fecha;
     private Sucursal sucursal;
-
-    public Venta() {
-    }
-
-    public Venta(Long id, Double total, LocalDate fecha, Sucursal sucursal) {
-        this.id = id;
-        this.total = total;
-        this.fecha = fecha;
-        this.sucursal = sucursal;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Double getTotal() {
-        return total;
-    }
-
-    public void setTotal(Double total) {
-        this.total = total;
-    }
-
-    public LocalDate getFecha() {
-        return fecha;
-    }
-
-    public void setFecha(LocalDate fecha) {
-        this.fecha = fecha;
-    }
-
-    public Sucursal getSucursal() {
-        return sucursal;
-    }
-
-    public void setSucursal(Sucursal sucursal) {
-        this.sucursal = sucursal;
-    }
 }
