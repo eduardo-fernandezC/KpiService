65c4d8f7a597fd3ba4b1f28fb38680011132ce12
Rocio Mino <roc.mino@duocuc.cl>
2026-04-22 17:58:32 -0400

    feat: agregar DetalleVenta a DataClient

---
+++ src/main/java/com/KpiService/model/Producto.java
@@ -0,0 +1,15 @@
+package com.KpiService.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Producto {
+
+    private String nombre;
+}
